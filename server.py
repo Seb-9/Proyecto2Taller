62b087c56e5031ca702c90e9b0b383e250d46a18
Seb-9 <56333750+Seb-9@users.noreply.github.com>
2020-08-18 17:50:39 -0600

    Update server.py

--- server.py
+++ server.py
@@ -9,39 +9,48 @@ ADDR = (SERVER,PORT) #establece la direccion IPv4 y el puerto a usar en el servi
 HEADER = 64 #cantidad de bytes aceptables en un paquete
 FORMAT = "utf-8" #formato para convertir a paquetes comunicables de bytes
 DISCONNECT_MESSAGE = "!DISCONNECT" #se usa para eliminar cualquier hilo en desuso de los clientes
-
+CONNECTIONS=[]
+TURNO=0
+insertard=0
 server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind(ADDR)
 
 def handle_client(conn,addr): #Controla cada instancia de cliente que ingrese
     print(f"[New connection] {addr} connected.\n")
-
+    CONNECTIONS.append(conn)
+    TURNO=CONNECTIONS.index(conn)
     connected = True
     while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-                
-            print(f"[{addr}] {msg}")
-            #conn.send(msg.encode(FORMAT))
-            if msg == "STFUTURE":
-
-            if msg == "DEFUSE"
-        send(msg,conn)
+        if TURNO==CONNECTIONS.index(conn):
+            msg_length = conn.recv(HEADER).decode(FORMAT)
+            if msg_length:
+                msg_length = int(msg_length)
+                msg = conn.recv(msg_length).decode(FORMAT)
+                if msg == DISCONNECT_MESSAGE:
+                    connected = False
+                else:
+                    answer(conn,msg,TURNO)
             
     conn.close()
-
-def send(msg,conn):
-    message = msg.encode(FORMAT)#Se debe codificar en un formato transferible(utf-8)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b" " * (HEADER - len(send_length))#se rellenan los espacios vacios
-                                                     #necesarios para el largo que acepta el cliente en el mensaje
-    conn.send(send_length)
-    conn.send(message)
+def answer(conn,msg,TURNO):
+    while TURNO<4:
+        TURNO+=1
+    else:
+        TURNO=0
+    if msg=="GIVE":
+        answer=Cartas_jugables.pop(random.randint(0,len(Cartas_jugables)))
+    if msg=="TAKE":
+        answer=CARTAS_baraja[-1]
+    if msg=="GIVEd":
+        answer=DEFUSE.pop(-1)
+        insertard+=1
+    if insertard==4:
+        CARTAS_jugables=CARTAS_jugables+DEFUSE
+        CARTAS_baraja=CARTAS_jugables+BOMB
+    else:
+        answer="p"
+    conn.send(answer.encode(FORMAT))
+    
     
 def start(): #espera por conexiones para pasarlas a handle_client
     server.listen()#escucha para las conexiones
@@ -51,41 +60,26 @@ def start(): #espera por conexiones para pasarlas a handle_client
         thread = threading.Thread(target=handle_client,args=(conn,addr))#genera un nuevo hilo para cada cliente que se conecte, esto para que dos se puedan conectar al mismo tiempo sin uno de los dos ser rechazado
         thread.start()#inicia el hilo anterior
         print(f"[Active connections] {threading.activeCount() - 2}")
-    
 
 print("Server starting...")
 start()
 
-####Parte del juego###
-#Se encarga de administrar las catas, los movimientos de los jugadores
+#Se encarga de administrar las cartas, los movimientos de los jugadores
 #y otros que requieran visualizacion de todos los jugadores
 
 #CARTAS, no se cargan imagenes ya que es irrelevante, solo identificadores
 #para los tipos de cartas
-NOPE=[n1,n2,n3,n4,n5]
-PASAR=[p1,p2,p3,p4]
-ATAQUE=[a1,a2,a3,a4]
-DEFUSE=[d1,d2,d3,d4,d5,d6]
-COMODIN=[c11,c12,c13,c14,c21,c22,c23,c24,c31,c32,c33,c34,c41,c42,c43,c44,c51,c52,c53,c54]
-BOMB=[b1,b2,b3,b4]
-FAVOR=[f1,f2,f3,f4]
-SHUFFLE=[s1,s2,s3,s4]#/barajar
-STFUTURE=[stf1,stf2,stf3,stf4,stf5]#/ver el futuro
+NOPE=["NOPE" for x in range(5)]
+PASAR=["SKIP" for x in range(4)]
+ATAQUE=["ATTACK" for x in range(4)]
+DEFUSE=["DEFUSE" for x in range(6)]
+COMODIN=["COMODIN1","COMODIN1","COMODIN1","COMODIN1","COMODIN2","COMODIN2","COMODIN2","COMODIN2","COMODIN3","COMODIN3","COMODIN3","COMODIN3","COMODIN4","COMODIN4","COMODIN4","COMODIN4","COMODIN5","COMODIN5","COMODIN5","COMODIN5"]
+BOMB=["BOMB" for x in range(4)]
+FAVOR=["FAVOR" for x in range(4)]
+SHUFFLE=["SHUFFLE" for x in range(4)]#/barajar
+STFUTURE=["STFUTURE" for x in range(5)]#/ver el futuro
 
-CARTAS_baraja = DEFUSE+COMODIN+BOMB+FAVOR+SHUFFLE+STFUTURE
-CARTAS_jugables = DEFUSE+COMODIN
-#cuando cada cliente se conecte, y se inicie el juego, se hacen los ajustes
-#a la cantidad de cartas que se repartiran en el mazo
-#se le dad a cada uno una respuesta con cartas
-
-#for jugador in jugadores:
-    # reparticion de cartas
-#while limite>0:
-	#i=random.randint(0,10)
-        #j=random.randint(0,10)
-        #jcartas.append(cartas[i][j])
-	#limite-=1
-#mezcla de la baraja
-    #random.shuffle(baraja)
+CARTAS_jugables = COMODIN+STFUTURE+SHUFFLE+ATAQUE+NOPE+FAVOR+PASAR
+random.shuffle(CARTAS_jugables)
 
  
